77cbc9f8872b60bbea3d02000eb4c5e250a1a6fd
Andrew Pinkham <code@andrewsforge.com>
2016-02-09 11:13:53 -0500

    Update env.sh and add unenv.sh to template.

--- {{ cookiecutter.project_slug }}/env.sh
+++ {{ cookiecutter.project_slug }}/env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export DOMAIN='{{ domain_name }}'
+export DOMAIN='{{ domain_name|lower }}'
 export AWS_ACCESS_KEY_ID='{{ aws_access_key_id }}'
 export AWS_SECRET_ACCESS_KEY='{{ aws_secret_access_key }}'
---
+++ {{ cookiecutter.project_slug }}/unenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+unset DOMAIN
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
