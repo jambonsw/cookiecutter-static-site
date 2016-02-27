a7e71343cbeb6af21cbfcc039ceb2a6db1bc0984
Andrew Pinkham <code@andrewsforge.com>
2016-02-27 14:09:21 -0500

    Add AWS region to environment.

--- {{ cookiecutter.project_slug }}/env.sh
+++ {{ cookiecutter.project_slug }}/env.sh
@@ -3,3 +3,4 @@
 export DOMAIN='{{ cookiecutter.domain_name|lower }}'
 export AWS_ACCESS_KEY_ID='{{ cookiecutter.aws_access_key_id }}'
 export AWS_SECRET_ACCESS_KEY='{{ cookiecutter.aws_secret_access_key }}'
+export AWS_DEFAULT_REGION='us-east-1'
